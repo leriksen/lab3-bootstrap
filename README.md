4f073ac21585856d26991c359d6a8f976ecf5e02
Leif Eriksen <leif.eriksen.au@gmail.com>
2020-10-23 12:10:23 +1100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lab3-bootstrap
 bootstrap the devops components of the Lab3 demo - github, azdo etc
+
+## Assumptions
+
+* terraform.io workspace `lab3-bootstrap` already setup in the `leif-lab3` org namespace
+* a service principal in Azure that can create Azure DevOps projects, pipelines, variable groups etc
+
+## Provides
+
+* a git repo for the demo project
+* an azure devops project with a pipeline and a service principal
---
+++ bootstrap.tf
@@ -0,0 +1,48 @@
+######### TERRAFORM BACKEND ##########
+terraform {
+  required_version = ">= 0.13, < 0.14"
+  required_providers {
+    azuredevops = {
+      source = "terraform-providers/azuredevops"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "leif-lab3"
+
+    workspaces {
+      name = "lab3-bootstrap"
+    }
+  }
+}
+
+######### AZURE DEVOPS SERVICE ##########
+
+# no version constraint - only at v0.0.1
+provider "azuredevops" {}
+
+######### RESOURCES ##########
+resource "azuredevops_project" "lab3_demo" {
+  project_name       = "lab3-demo"
+  description        = "Demonstrate some capabilities to Lab3"
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
+
+  features = {
+    boards       = "disabled"
+    repositories = "enabled"
+    pipelines    = "enabled"
+    testplans    = "disabled"
+    artifacts    = "disabled"
+  }
+}
+
+resource "azuredevops_git_repository" "lab3_demo_repo" {
+  project_id = azuredevops_project.lab3_demo.id
+  name       = "lab3-demo"
+  initialization {
+    init_type = "Clean"
+  }
+}
